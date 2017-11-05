e85b789ab4211f80e38b66b515e85eb305fafc49
cc <candyer@users.noreply.github.com>
2017-11-05 15:03:20 +0100

    Pressing the Enter key

---
+++ event_binding/src/app/input/input.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'todo-input',
+	template: `
+	<input [value]="title" 
+			(keyup.enter)="changeTitle($event.target.value)">
+		<button (click)="changeTitle('Button Clicked!')">
+			Save
+		</button>
+	<p>The title is: {{ title }}</p>
+	`,
+	styleUrls: ['./input.component.css']
+})
+
+export class InputComponent implements OnInit {
+	title: string = 'My First Todo Title !!!';           
+
+	constructor() { }                     
+
+	ngOnInit() {
+	}
+
+	changeTitle(newTitle: string): void {
+		this.title = newTitle;
+	}
+
+}
