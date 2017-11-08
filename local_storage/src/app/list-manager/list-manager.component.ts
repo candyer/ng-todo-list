7ab780e09fc5ffe638ec56fbec0d94f00553b244
cc <candyer@users.noreply.github.com>
2017-11-08 14:50:14 +0100

    Local storage

---
+++ local_storage/src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoListService } from '../todo-list.service';
+
+@Component({
+	selector: 'todo-list-manager',
+	template: `
+	<h1>
+	{{title}}
+	</h1>
+
+	<todo-input (submit)="addItem($event)"></todo-input>
+	<ul>
+		<li *ngFor="let item of todoList">
+			<todo-item [todoItem]="item"></todo-item>
+		</li>
+	</ul>
+	`,
+	styleUrls: ['./list-manager.component.css']
+})
+export class ListManagerComponent implements OnInit {
+	todoList = [];
+
+	addItem(title:string) {
+		this.todoList = this.todoListService.addItem({ title });
+	}
+
+	constructor(private todoListService:TodoListService) { }
+
+	ngOnInit() {
+		this.todoList = this.todoListService.getTodoList();
+	}
+
+}
